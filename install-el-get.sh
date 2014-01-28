7a97ccfd520d72a4c577807186ed7b70ecdec8a4
Joerg Ziefle <joerg.ziefle@gmail.com>
2014-01-28 21:06:46 +0100

    Patch is necessary for now.

--- install-el-get.sh
+++ install-el-get.sh
@@ -4,4 +4,5 @@ EMACS_D_DIR=$HOME/.emacs.d
 EL_GET_DIR=$EMACS_D_DIR/el-get
 
 mkdir $EL_GET_DIR \
-	&& git clone https://github.com/dimitri/el-get.git $EL_GET_DIR/el-get/
+	&& git clone https://github.com/dimitri/el-get.git $EL_GET_DIR/el-get/ \
+	&& patch -d $EL_GET_DIR/el-get < $EMACS_D_DIR/el-get.patch
